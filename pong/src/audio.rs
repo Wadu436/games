2eab73df87ccda75c22e2e44567fc3c080f564b5
Warre Dujardin <warre.dujardin@gmail.com>
2024-06-20 18:04:48 +0200

    do I know what I'm doing? not really

--- pong/src/audio.rs
+++ pong/src/audio.rs
@@ -2,24 +2,17 @@ use std::io::Read;
 use std::sync::{Arc, Mutex};
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::BufferSize;
 use rubato::{
-    Resampler, SincFixedIn, SincInterpolationParameters, SincInterpolationType, WindowFunction,
+    Resampler, SincFixedOut, SincInterpolationParameters, SincInterpolationType, WindowFunction,
 };
 pub struct AudioSystem {
     _stream: cpal::Stream,
-    sounds: Arc<Mutex<Vec<Sound>>>,
+    sounds: Arc<Mutex<Vec<SoundInstance>>>,
     sample_rate: f64,
 }
 
-#[derive(Debug, Clone)]
-pub struct Sound {
-    data: Vec<f32>,
-    _channels: u16,
-    _sample_rate: u32,
-    i: usize,
-    repeat: bool,
-    playing: bool,
-}
+const BUFFER_SIZE_PER_CHANNEL: usize = 1024;
 
 impl AudioSystem {
     pub fn new() -> eyre::Result<Self> {
@@ -30,30 +23,90 @@ impl AudioSystem {
         println!("Output device: {}", device.name()?);
         let config = device.default_output_config().unwrap();
         println!("Default output config: {:?}", config);
+        let mut stream_config = config.config();
 
-        let sample_rate = config.config().sample_rate.0 as f64;
+        let sample_rate = stream_config.sample_rate.0 as f64;
+        stream_config.buffer_size =
+            BufferSize::Fixed(BUFFER_SIZE_PER_CHANNEL as u32 * stream_config.channels as u32);
 
-        let sounds = Arc::new(Mutex::new(Vec::<Sound>::new()));
+        let sounds = Arc::new(Mutex::new(Vec::<SoundInstance>::new()));
         let sounds_stream = sounds.clone();
 
         let stream = device.build_output_stream(
-            &config.config(),
+            &stream_config,
             move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                println!("Audio Thread: requesting {} samples", data.len());
                 let mut sounds_lock = sounds_stream.lock().unwrap();
+                println!("Audio Thread: Acquired lock");
                 for sample in data.iter_mut() {
                     *sample = 0.0;
                 }
-                for sound in sounds_lock.iter_mut().filter(|sound| sound.playing) {
-                    for sample in data.iter_mut() {
-                        *sample += sound.data[sound.i];
+                for (i, sound) in sounds_lock
+                    .iter_mut()
+                    .filter(|sound| sound.playing)
+                    .enumerate()
+                {
+                    println!("Sound: {}", i);
+                    for (i, sample) in data.iter_mut().enumerate() {
+                        let channel = i % stream_config.channels as usize;
 
-                        sound.i += 1;
-                        if sound.i >= sound.data.len() {
-                            sound.i = 0;
-                            if !sound.repeat {
-                                sound.playing = false;
-                                break;
+                        if sound.resampled_buffer_index >= BUFFER_SIZE_PER_CHANNEL {
+                            // Resample
+                            let num_samples = sound.resampler.input_frames_next();
+                            // Check if we have enough samples
+                            if sound.i + num_samples <= sound.sound.samples_channels[0].len() {
+                                let (consumed, _) = sound
+                                    .resampler
+                                    .process_into_buffer(
+                                        &[
+                                            &sound.sound.samples_channels[0]
+                                                [sound.i..sound.i + num_samples],
+                                            &sound.sound.samples_channels[1]
+                                                [sound.i..sound.i + num_samples],
+                                        ],
+                                        sound.resampled_buffer.as_mut(),
+                                        None,
+                                    )
+                                    .unwrap();
+                                sound.resampled_buffer_index = 0;
+                                sound.i += consumed;
+                            } else if sound.i <= sound.sound.samples_channels[0].len() {
+                                let (consumed, _) = sound
+                                    .resampler
+                                    .process_partial_into_buffer(
+                                        Some(&[
+                                            &sound.sound.samples_channels[0][sound.i..],
+                                            &sound.sound.samples_channels[1][sound.i..],
+                                        ]),
+                                        sound.resampled_buffer.as_mut(),
+                                        None,
+                                    )
+                                    .unwrap();
+                                sound.resampled_buffer_index = 0;
+                                sound.i += consumed;
+                            } else {
+                                let (consumed, _) = sound
+                                    .resampler
+                                    .process_partial_into_buffer(
+                                        None::<&[&[f32]]>,
+                                        sound.resampled_buffer.as_mut(),
+                                        None,
+                                    )
+                                    .unwrap();
+                                sound.resampled_buffer_index = 0;
+                                sound.i += consumed;
+                                if sound.sound.repeat {
+                                    sound.i = 0;
+                                } else {
+                                    sound.playing = false;
+                                }
                             }
+                        }
+
+                        *sample = sound.resampled_buffer[channel][sound.resampled_buffer_index];
+
+                        if channel == stream_config.channels as usize - 1 {
+                            sound.resampled_buffer_index += 1;
                         }
                     }
                 }
@@ -72,7 +125,8 @@ impl AudioSystem {
 
     pub fn play_sound(&self, sound: Sound) {
         // Play sound
-        self.sounds.lock().unwrap().push(sound);
+        let sound_instance = self.create_sound_instance(sound);
+        self.sounds.lock().unwrap().push(sound_instance);
     }
 
     pub fn load_sound<R: Read>(&self, reader: R, repeat: bool) -> eyre::Result<Sound> {
@@ -120,15 +174,24 @@ impl AudioSystem {
                     .collect::<Vec<f32>>()
             })
             .collect();
-        samples_channels
-            .iter()
-            .for_each(|c| println!("Channel length: {}", c.len()));
-
         if samples_channels.len() == 1 {
             // mono sound
             samples_channels.push(samples_channels[0].clone());
         }
 
+        let samples_channels = [samples_channels[0].clone(), samples_channels[1].clone()];
+
+        println!("Samples length: {}", samples.len());
+
+        // Load sound
+        Ok(Sound {
+            samples_channels: Arc::new(samples_channels),
+            sample_rate: spec.sample_rate as f32,
+            repeat,
+        })
+    }
+
+    pub fn create_sound_instance(&self, sound: Sound) -> SoundInstance {
         let params = SincInterpolationParameters {
             sinc_len: 256,
             f_cutoff: 0.95,
@@ -136,37 +199,41 @@ impl AudioSystem {
             oversampling_factor: 256,
             window: WindowFunction::BlackmanHarris2,
         };
-        let mut resampler = SincFixedIn::<f32>::new(
-            self.sample_rate / spec.sample_rate as f64,
+        let resampler = SincFixedOut::<f32>::new(
+            self.sample_rate / sound.sample_rate as f64,
             2.0,
             params,
-            samples_channels[0].len(),
+            BUFFER_SIZE_PER_CHANNEL,
             2,
         )
         .unwrap();
 
-        let samples_channels = resampler.process(&samples_channels, None).unwrap();
-
-        let samples = {
-            let mut samples: Vec<f32> = Vec::with_capacity(samples_channels[0].len() * samples_channels.len());
-            for i in 0..samples_channels[0].len() {
-                for c in 0..samples_channels.len() {
-                    samples.push(samples_channels[c as usize][i]);
-                }
-            }
-            samples
-        };
-
-        println!("Samples length: {}", samples.len());
-
-        // Load sound
-        Ok(Sound {
-            data: samples,
-            _channels: spec.channels,
-            _sample_rate: spec.sample_rate,
+        SoundInstance {
+            sound,
+            resampler,
+            resampled_buffer: [
+                [0.0; BUFFER_SIZE_PER_CHANNEL],
+                [0.0; BUFFER_SIZE_PER_CHANNEL],
+            ],
+            resampled_buffer_index: BUFFER_SIZE_PER_CHANNEL,
             i: 0,
-            repeat,
             playing: true,
-        })
+        }
     }
+}
+
+#[derive(Clone)]
+pub struct Sound {
+    samples_channels: Arc<[Vec<f32>; 2]>,
+    sample_rate: f32,
+    repeat: bool,
+}
+
+pub struct SoundInstance {
+    sound: Sound,
+    resampled_buffer: [[f32; BUFFER_SIZE_PER_CHANNEL]; 2],
+    resampled_buffer_index: usize,
+    resampler: SincFixedOut<f32>,
+    i: usize,
+    playing: bool,
 }
